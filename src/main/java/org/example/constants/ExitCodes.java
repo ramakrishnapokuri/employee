6777a4dca174864b7de6702324df3c679ec48c2d
ramakrishnapokuri <ramakrishnapokuri@gmail.com>
2025-12-12 19:01:10 +0530

    Create ExitCodes.java

---
+++ src/main/java/org/example/constants/ExitCodes.java
@@ -0,0 +1,11 @@
+package org.example.constants;
+
+public class ExitCodes {
+
+    public static final int SUCCESS = 0;
+    public static final int INVALID_INPUT = 1;
+    public static final int ANALYSIS_ERROR = 2;
+    public static final int REPORTING_ERROR = 3;
+    public static final int UNEXPECTED_ERROR = 4;
+
+}
